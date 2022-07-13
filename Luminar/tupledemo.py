5d03d5a2659eff73466cf5cdfa5a0c78f9938dfc
Gokul Das <gokuldas1999@gmail.com>
2022-07-13 11:50:18 +0530

    new files

---
+++ Luminar/tupledemo.py
@@ -0,0 +1,12 @@
+# ()
+# duplication allowed
+# insertion order preserved
+# length dynamic
+
+expenses=(10000,12000,13000,14000)
+print(expenses[0])
+
+# expenses[3]=40000     cant edit or assign
+# print(expenses)
+
+# print(dir(expenses))
