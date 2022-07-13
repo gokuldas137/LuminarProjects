d2e2f57d37c480e61e6d79883ce64e6a67eb1c77
Gokul Das <gokuldas1999@gmail.com>
2022-07-13 11:41:17 +0530

    new files

--- Luminar/products1.py
+++ Luminar/products1.py
@@ -11,20 +11,90 @@ mobiles = [
     [1009, "iphone13", "5g", "AMOLED", 97000, "apple", 0],
     [1010, "oneplusnordce2", "5g", "AMOLED", 23000, "oneplus", 67],
 ]
-# 1.Total number of mobiles
-print(f"Total number of mobiles{len(mobiles)}")
+# # 1.Total number of mobiles
+# print(f"Total number of mobiles{len(mobiles)}")
 
 
-# q1 - total no: of out of stock mobiles
-# q2 - total stock
-# q3 - print mobiles available in range 20K to 30K
-# q4 - print all 5G phones
-# q5 - print samsung mobiles
-# q6 - print costly mobile
+# # q1 - total no: of out of stock mobiles
+# outofstock=[mob for mob in mobiles if mob[-1]==0]
+# print(outofstock)
+
+
+
+# # q2 - total no of out of stock
+# print("No of out of stock ",len(outofstock))
+
+
+# # total stock
+# totalstock=[mob[-1] for mob in mobiles]  #mapping
+# print("total stock ",sum(totalstock))
+
+
+
+# # q3 - print mobiles available in range 20K to 30K
+# pricerange=[mob for mob in mobiles if mob[4] in range(20000,30000)]
+# print("between ",pricerange)
+
+
+
+# # q4 - print all 5G phones
+# mobs=[mob for mob in mobiles if mob[2]=="5g"]
+# print("5g ",mobs)
+#
+# # q5 - print samsung mobiles
+# mobs=[mob for mob in mobiles if mob[-2]=="samsung"]
+# print(mobs)
+
+
+
+# # q6 - print costly mobile
+# max_prices=max([mob[4] for mob in mobiles])
+# costly=[mob for mob in mobiles if mob[4]==max_prices]
+# print("\n costly",costly)
+#
+
 # q7 - print low cost mobile
-# q8 - print all mobiles having stock > 10
-# q9 - count of mobiles having display amoled
-# q10 - sort mobiles based on price order by descending
+# low_cost=min(mobiles,key=lambda m:m[4])
+# print(low_cost)
+#
+#
+# # q8 - print all mobiles having stock > 10
+#  mobiles=[mob for mob in mobiles if mob[-1]>10]
+# print(mobs)
+#
+#
+# # q9 - count of mobiles having display amoled
+#
+# count=[mob for mob in mobiles if mob[3]=="AMOLED"]
+# print(len(count))
+#
+#
+# # # q10 - sort mobiles based on price order by ascending
+# mobiles.sort(reverse=False,key=lambda m:m[4])
+# print(mobiles)
+
+#
 # q11 - sort mobiles based on avl stock order by descending
+# (mobiles.sort(reverse=True,key=lambda m:m[-1]))
+# print(mobiles)
+
+
 # q13 - most available stock
+# avl_stck=max(mobiles,key=lambda m:m[-1])
+# print(avl_stck)
+
+
 # q14 - least available stock
+# avl_stckmin=min(mobiles,key=lambda m:m[-1])
+# print(avl_stckmin)
+
+
+# sort
+# mobiles.sort(reverse=True,key=lambda m:m[4])
+# print(mobiles)
+
+
+
+# is there any mobile available at rs 10000
+mobten=[mob[4]==10000 for mob in mobiles]
+print("available" if True in mobten else "no")
