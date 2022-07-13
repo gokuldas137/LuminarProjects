24dc5ec63347d4c5e975951791bd15a687692e17
Gokul Das <gokuldas1999@gmail.com>
2022-07-13 10:33:06 +0530

    new files

--- Luminar/nestedlist.py
+++ Luminar/nestedlist.py
@@ -2,7 +2,8 @@ lst = [
     [10, 11],
     [13, 45],
     [50, 15],
-    [60, 16]
+    [60, 16],
+    [70, 60]
 ]
 
 # for sub_lst in lst:
@@ -21,3 +22,13 @@ for sub_lst in lst:
     for num in sub_lst:
         flatten_lst.append(num)
 print(max(flatten_lst))
+
+
+odd=[num for num in flatten_lst if num%2!=0]
+print(odd)
+
+even=[num for num in flatten_lst if num%2==0]   #filter
+print(sum(even))
+
+mappedlist=[num+1 if num>25 else num-1 for num in flatten_lst] #mapped list
+print(mappedlist)
