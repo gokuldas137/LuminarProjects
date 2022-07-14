c4f038c3a9500eae19736f4f4a01603c673be183
Gokul Das <gokuldas1999@gmail.com>
2022-07-14 15:29:07 +0530

    new files

--- Luminar/Dictionary/most_recursive_element.py
+++ Luminar/Dictionary/most_recursive_element.py
@@ -4,10 +4,13 @@ pattern="ABACCDA"
 
 a={}
 word_count={"a":2,"b":5,"c":2,"d":4,"e":2}
+print(max(word_count.items(),key=lambda i:i[1]))
+print(min(word_count.items(),key=lambda i:i[1]))
 
-wc_list=word_count.items()
-print(sorted(wc_list,key=lambda item:item[1],reverse=True))     #sorting in dictionary
 
-#max()
-
-print(max(wc_list,key=lambda item:item[1]))
+# wc_list=word_count.items()
+# print(sorted(wc_list,key=lambda item:item[1],reverse=True))     #sorting in dictionary
+#
+# #max()
+#
+# print(max(wc_list,key=lambda il:l[1]))
